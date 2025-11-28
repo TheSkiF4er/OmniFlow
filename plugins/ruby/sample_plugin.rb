3e188a554988560b36a8cdbfcd3e5f4c621d5c8e
Dmitry Efimov <cajeerskif4er@gmail.com>
2025-11-28 21:16:10 +0300

    Update sample_plugin.rb

--- plugins/ruby/sample_plugin.rb
+++ plugins/ruby/sample_plugin.rb
@@ -1,1 +1,37 @@
+#!/usr/bin/env ruby
+require 'json'
 
+# OmniFlow Ruby Plugin
+# This plugin multiplies a number by 2 and returns the result
+#
+# Input example:
+# {
+#   "number": 5
+# }
+#
+# Output example:
+# {
+#   "message": "Ruby plugin executed successfully!",
+#   "result": 10
+# }
+
+# Read input JSON from stdin
+input = STDIN.read
+begin
+  event = JSON.parse(input)
+rescue JSON::ParserError
+  event = {}
+end
+
+# Process the event
+number = event["number"]
+result = number.is_a?(Numeric) ? number * 2 : nil
+
+# Prepare output
+output = {
+  "message" => "Ruby plugin executed successfully!",
+  "result" => result
+}
+
+# Write JSON output to stdout
+puts JSON.pretty_generate(output)
