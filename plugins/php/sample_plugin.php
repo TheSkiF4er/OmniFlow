f2e20ea70f4fa9cb9944aafd7280dca6a1d31085
Dmitry Efimov <cajeerskif4er@gmail.com>
2025-11-28 21:13:33 +0300

    Update sample_plugin.php

--- plugins/php/sample_plugin.php
+++ plugins/php/sample_plugin.php
@@ -1,1 +1,34 @@
+<?php
+/**
+ * OmniFlow PHP Plugin
+ * This plugin multiplies a number by 2 and returns the result
+ *
+ * Input example:
+ * {
+ *   "number": 5
+ * }
+ *
+ * Output example:
+ * {
+ *   "message": "PHP plugin executed successfully!",
+ *   "result": 10
+ */
 
+// Read input from stdin
+$input = file_get_contents('php://stdin');
+$data = json_decode($input, true);
+
+// Initialize result
+$result = null;
+if (isset($data['number']) && is_numeric($data['number'])) {
+    $result = $data['number'] * 2;
+}
+
+// Prepare output
+$output = [
+    'message' => 'PHP plugin executed successfully!',
+    'result' => $result
+];
+
+// Return output as JSON
+echo json_encode($output, JSON_PRETTY_PRINT);
