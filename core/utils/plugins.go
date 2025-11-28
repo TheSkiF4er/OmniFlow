37ba06bb6703026952ec831966add2664866c71b
Dmitry Efimov <cajeerskif4er@gmail.com>
2025-11-28 23:18:16 +0300

    Update plugins.go

--- core/utils/plugins.go
+++ core/utils/plugins.go
@@ -1,1 +1,12 @@
+package utils
 
+import (
+	"os/exec"
+)
+
+// RunPlugin executes a plugin as an external process
+func RunPlugin(path string, args ...string) ([]byte, error) {
+	cmd := exec.Command(path, args...)
+	output, err := cmd.CombinedOutput()
+	return output, err
+}
