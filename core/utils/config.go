5679df8530f0f57f0a16dab90136e3c6e32f962a
Dmitry Efimov <cajeerskif4er@gmail.com>
2025-11-28 23:16:19 +0300

    Update config.go

--- core/utils/config.go
+++ core/utils/config.go
@@ -1,1 +1,28 @@
+package utils
 
+import (
+	"encoding/json"
+	"os"
+)
+
+type Config struct {
+	DatabaseURL string `json:"database_url"`
+	Port        int    `json:"port"`
+	LogLevel    string `json:"log_level"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	cfg := &Config{}
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(cfg)
+	if err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
