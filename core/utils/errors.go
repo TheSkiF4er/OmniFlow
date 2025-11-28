55d6b0c23d23c6d20fa70931539350bead803b1b
Dmitry Efimov <cajeerskif4er@gmail.com>
2025-11-28 23:17:08 +0300

    Update errors.go

--- core/utils/errors.go
+++ core/utils/errors.go
@@ -1,1 +1,16 @@
+package utils
 
+import "fmt"
+
+type OmniError struct {
+	Code    int
+	Message string
+}
+
+func (e OmniError) Error() string {
+	return fmt.Sprintf("Code %d: %s", e.Code, e.Message)
+}
+
+func NewOmniError(code int, msg string) error {
+	return OmniError{Code: code, Message: msg}
+}
