443f4874c5b123320d1302f04e1f5b7738300a22
Dmitry Efimov <cajeerskif4er@gmail.com>
2025-11-28 23:19:08 +0300

    Update helpers.go

--- core/utils/helpers.go
+++ core/utils/helpers.go
@@ -1,1 +1,26 @@
+package utils
 
+import (
+	"time"
+)
+
+// SleepSeconds pauses execution for n seconds
+func SleepSeconds(n int) {
+	time.Sleep(time.Duration(n) * time.Second)
+}
+
+// Min returns the minimum of two integers
+func Min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// Max returns the maximum of two integers
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
